8d9e1a544636e548b72caa2bd804d7f2a49882d2
rakesh0001-mongo <rakeshk7575ew@gmail.com>
2025-08-06 18:35:44 +0530

    Update info.py

--- info.py
+++ info.py
@@ -21,7 +21,7 @@ BOT_TOKEN = environ.get('BOT_TOKEN', '7970491776:AAHyIsYmaJChVSDeF4NZXaDmruttkv6
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '8426034007').split()]
 USERNAME = environ.get('USERNAME', "https://t.me/Rakesh0001_rk") # ADMIN USERNAME
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002793410950'))
-MOVIE_GROUP_LINK = environ.get('MOVIE_GROUP_LINK', 'https://t.me/+K2kgCBgaat80YWQ9')
+MOVIE_GROUP_LINK = environ.get('MOVIE_GROUP_LINK', 'https://t.me/rksearchinggroup')
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002741089344').split()]
 #---------------------------------------------------------------
 #---------------------------------------------------------------
